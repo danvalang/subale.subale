7d8a5984d68ad3e7fb1e22109044af7e61097942
danvalang <hittemni@gmail.com>
2012-10-06 01:54:58 -0500

    Inicio
    Signed-off-by: danvalang <hittemni@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubaleSubale::Application.config.secret_token = '99529e7b3520e6f0f862bf475d3eb4f659e2fde16c4801ba63e3c8d7d36efdbac9d48342472ee3cf6bb125db900a07b80e454167b4f9fd96ad241d916d9200b1'
